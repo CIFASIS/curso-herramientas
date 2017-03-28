72a5bbd791d7423b64e7398de6ffaab7a7a45087
gustavo <g@g>
2017-03-28 10:35:45 -0300

    mejorados ejercicios y soluciones

--- 2/churn.py
+++ 2/churn.py
@@ -7,6 +7,21 @@ from sklearn import preprocessing
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.manifold import Isomap, TSNE
 
+data = pd.read_csv('data/churn.csv')
+
+print data.head()
+print data.describe()
+
+y = data['Churn?'] == "True."
+
+to_drop = ['State','Area Code','Phone','Churn?']
+X = data.drop(to_drop,axis=1)
+
+yes_no_cols = ["Int'l Plan","VMail Plan"]
+X[yes_no_cols] = X[yes_no_cols] == 'yes'
+
+print X.head()
+
 # ----
 # ejercicios
 # ----
--- soluciones/4/churn.py
+++ soluciones/4/churn.py
@@ -6,7 +6,7 @@ import matplotlib.colors as colors
 from sklearn import preprocessing
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split, cross_val_score
 
@@ -21,11 +21,13 @@ X = data.drop(to_drop,axis=1)
 yes_no_cols = ["Int'l Plan","VMail Plan"]
 X[yes_no_cols] = X[yes_no_cols] == 'yes'
 
+X_train, X_test, y_train, y_test = train_test_split(X,y)
+
 scaler = preprocessing.StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-X_train, X_test, y_train, y_test = train_test_split(X_scaled,y)
-
+print X_train.shape
 
 clf = RandomForestClassifier()
 clf.fit(X_train,y_train)
@@ -33,8 +35,17 @@ clf.fit(X_train,y_train)
 print "RandomForestClassifier score:", accuracy_score(y_test, clf.predict(X_test), normalize=True)
 print  confusion_matrix(y_test, clf.predict(X_test))
  
+best_score = 0.0
+
 for C in [0.1,1,10,100,1000]:
     for tol in [0.00001, 0.00001, 0.0001, 0.001, 0.01]:
-        clf = SVC(kernel="linear", C=C, tol=tol)
-        score = cross_val_score(clf, X_train, y_train, cv=5)
-        print "SVM score:", score.mean()
+        clf = LinearSVC(C=C, tol=tol)
+        score = cross_val_score(clf, X_train, y_train, cv=5).mean()
+        if best_score < score:
+            best_score = score
+            print "Best SVM valid score:", score
+            best_params = dict(C=C, tol=tol)
+
+clf = LinearSVC(**best_params)
+clf.fit(X_train,y_train)
+print "SVM test score:", accuracy_score(y_test, clf.predict(X_test), normalize=True) 
